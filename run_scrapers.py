87a6f65efd802c8dd582d3c22cd1136a1f89a9c5
Patrolin <patrik@balas.cz>
2025-02-18 17:04:07 +0100

    c

--- run_scrapers.py
+++ run_scrapers.py
@@ -1,14 +1,8 @@
-import traceback
 from scrape_steam_apps import scrape_steam_apps
 
 functions = [
   scrape_steam_apps
 ]
 if __name__ == '__main__':
-  with open("run_scrapers.log", "w+", encoding="utf8") as f:
-    try:
-      for function in functions:
-        function()
-    except BaseException as err:
-      err_string = "".join(traceback.format_exception(type(err), err, err.__traceback__))
-      f.write(f"exception: {err_string}\n")
+  for function in functions:
+    function()
